39b35076ef28017ea1c2c4f6ff111c78dc7910ec
OgnjenArsenijevic <30389716+OgnjenArsenijevic@users.noreply.github.com>
2019-03-16 14:42:47 +0100

    C++

---
+++ 151. Reverse Words in a String.cpp
@@ -0,0 +1,20 @@
+///151. Reverse Words in a String
+///Author: Ognjen Arsenijevic
+///username: ognjen1998
+public class Solution
+{
+    public String reverseWords(String s) 
+    {
+        s=s.trim();
+        if(s.length()==0) return s;
+        String ss="";
+        String[] split=s.split(" ");
+        for(int i=split.length-1;i>=0;i--)
+        {
+           split[i]=split[i].trim();
+           if(ss.length()>0 && split[i].length()>0) ss+=" ";
+             ss+=split[i];
+        }
+        return ss;
+    }
+}
