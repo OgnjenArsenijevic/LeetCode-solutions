b210d42b43f57cc0767824a1e2823afc8fa864e5
OgnjenArsenijevic <30389716+OgnjenArsenijevic@users.noreply.github.com>
2019-03-16 15:38:52 +0100

    C++

---
+++ 219. Contains Duplicate II.cpp
@@ -0,0 +1,18 @@
+///219. Contains Duplicate II
+///Author: Ognjen Arsenijevic
+///username: ognjen1998
+class Solution
+{
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) 
+    {
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++) 
+        {
+            if(m.count(nums[i]) && i-m[nums[i]]<=k) 
+                return true;
+            m[nums[i]]=i;
+        }    
+        return false;
+    }
+};
