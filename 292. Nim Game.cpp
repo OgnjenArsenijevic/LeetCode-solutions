8efd4710dda0acceb75df2d9c39c8c75fae43276
OgnjenArsenijevic <30389716+OgnjenArsenijevic@users.noreply.github.com>
2019-03-16 16:11:58 +0100

    C++

---
+++ 292. Nim Game.cpp
@@ -0,0 +1,12 @@
+///292. Nim Game
+///Author: Ognjen Arsenijevic
+///username: ognjen1998
+class Solution 
+{
+public:
+    bool canWinNim(int n) 
+    {
+        if(n%4) return true;
+        return false;
+    }
+};
