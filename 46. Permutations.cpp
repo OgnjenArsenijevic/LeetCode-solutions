14d3d34df11e04223c5794fcf7db6dd89e44dcd5
OgnjenArsenijevic <30389716+OgnjenArsenijevic@users.noreply.github.com>
2019-03-16 04:07:10 +0100

    C++

---
+++ 46. Permutations.cpp
@@ -0,0 +1,17 @@
+///46. Permutations
+///Author: Ognjen Arsenijevic
+///username: ognjen1998
+class Solution 
+{
+public:
+    vector<vector<int>> permute(vector<int>& v) 
+    {
+        vector<vector<int>> ans;
+        sort(v.begin(),v.end());
+        do 
+        {
+           ans.push_back(v);  
+        } while (next_permutation(v.begin(),v.end()));
+        return ans;
+    }
+};
