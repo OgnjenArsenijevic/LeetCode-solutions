4c62e3713ee844deeeabddcafbaf7c71d4d9932d
OgnjenArsenijevic <30389716+OgnjenArsenijevic@users.noreply.github.com>
2019-03-16 04:02:49 +0100

    C++

---
+++ 31. Next Permutation.cpp
@@ -0,0 +1,11 @@
+///31. Next Permutation
+///Author: Ognjen Arsenijevic
+///username: ognjen1998
+class Solution 
+{
+public:
+    void nextPermutation(vector<int>& v) 
+    {
+        next_permutation(v.begin(),v.end());
+    }
+};
