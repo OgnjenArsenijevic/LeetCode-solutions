d00d40994db9f5d9b3537af5b06f5d6d0b3013d9
OgnjenArsenijevic <30389716+OgnjenArsenijevic@users.noreply.github.com>
2019-03-16 14:07:32 +0100

    C++

---
+++ 58. Length of Last Word.cpp
@@ -0,0 +1,22 @@
+///58. Length of Last Word
+///Author: Ognjen Arsenijevic
+///username: ognjen1998
+class Solution 
+{
+public:
+    int lengthOfLastWord(string s) 
+    {
+        int cnt=0;
+        for(int i=s.size()-1;i>=0;i--)
+        {
+            if(s[i]!=' ')
+                cnt++;
+            else
+            {
+                if(cnt>0)
+                    break;
+            }
+        }
+        return cnt;
+    }
+};
