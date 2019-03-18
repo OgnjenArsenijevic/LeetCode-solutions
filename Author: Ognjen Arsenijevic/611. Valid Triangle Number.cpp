ad72c4c16df9f45764f8b7013e875086065d04bd
OgnjenArsenijevic <30389716+OgnjenArsenijevic@users.noreply.github.com>
2019-03-18 16:38:18 +0100

    C++

---
+++ Author: Ognjen Arsenijevic/611. Valid Triangle Number.cpp
@@ -0,0 +1,25 @@
+///611. Valid Triangle Number
+///Author: Ognjen Arsenijevic
+///username: ognjen1998
+class Solution 
+{
+public:
+    int triangleNumber(vector<int>& v)
+    {
+        if(v.size()<3) return 0;
+        sort(v.begin(),v.end());
+        int start=0,ans=0;
+        while(v[start]==0 && start<v.size()) start++;
+        for(int i=start;i<v.size()-2;i++)
+        {
+            int k=i+2;
+            for(int j=i+1;j<v.size()-1;j++)
+            {
+                while(k<v.size() && v[i]+v[j]>v[k])
+                    k++;
+                ans+=k-j-1;
+            }
+        }
+        return ans;
+    }
+};
