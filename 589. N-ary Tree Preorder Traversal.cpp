9591fd98aa8dcf7804af8bd26c3d137bcb707e0f
OgnjenArsenijevic <30389716+OgnjenArsenijevic@users.noreply.github.com>
2019-03-18 16:34:55 +0100

    C++

--- 589. N-ary Tree Preorder Traversal.cpp
+++ 589. N-ary Tree Preorder Traversal.cpp
@@ -1,3 +1,34 @@
 ///589. N-ary Tree Preorder Traversal
 ///Author: Ognjen Arsenijevic
 ///username: ognjen1998
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution 
+{
+public:
+    void solve(Node* root, vector<int> &ans)
+    {
+        if(root==nullptr) return;
+        ans.push_back(root->val);
+        for(int i=0;i<root->children.size();i++)
+            solve(root->children[i],ans);
+    }
+    vector<int> preorder(Node* root) {
+        vector<int> ans;
+        solve(root,ans);
+        return ans;
+    }
+};
