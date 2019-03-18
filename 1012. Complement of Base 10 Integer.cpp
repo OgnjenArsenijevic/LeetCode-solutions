952499f36fdc72388026179b9ea2280d44ca1c75
OgnjenArsenijevic <30389716+OgnjenArsenijevic@users.noreply.github.com>
2019-03-18 18:55:07 +0100

    C++

---
+++ 1012. Complement of Base 10 Integer.cpp
@@ -0,0 +1,17 @@
+///1012. Complement of Base 10 Integer
+///Author: Ognjen Arsenijevic
+///username: ognjen1998
+class Solution 
+{
+public:
+    int bitwiseComplement(int n) 
+    {
+        if(n==0) return 1;
+        long long curr=2LL;
+        while(curr<=n)
+        {
+            curr*=2;
+        }    
+        return curr-n-1;
+    }
+};
