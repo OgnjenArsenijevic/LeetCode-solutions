92c9832371cb82bf8f6ae9a70d9dd5c99b5e27ea
OgnjenArsenijevic <30389716+OgnjenArsenijevic@users.noreply.github.com>
2019-03-18 01:17:41 +0100

    C++

---
+++ 509. Fibonacci Number.cpp
@@ -0,0 +1,16 @@
+///509. Fibonacci Number
+///Author: Ognjen Arsenijevic
+///username: ognjen1998
+class Solution 
+{
+public:
+    int fib(int n) 
+    {
+        int f[35];
+        f[0]=0;
+        f[1]=1;
+        for(int i=2;i<=n;i++)
+            f[i]=f[i-1]+f[i-2];
+        return f[n];
+    }
+};
