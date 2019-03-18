160d5fb83cb5b2b283a1cc470eca660d880dd278
OgnjenArsenijevic <30389716+OgnjenArsenijevic@users.noreply.github.com>
2019-03-18 18:15:39 +0100

    C++

---
+++ 933. Number of Recent Calls.cpp
@@ -0,0 +1,26 @@
+///933. Number of Recent Calls
+///Author: Ognjen Arsenijevic
+///username: ognjen1998
+class RecentCounter 
+{
+public:
+    queue<int> q;
+    RecentCounter() 
+    {
+            
+    }
+    
+    int ping(int t) 
+    {
+        q.push(t);
+        while(q.front()<t-3000)
+            q.pop();
+        return q.size();
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
