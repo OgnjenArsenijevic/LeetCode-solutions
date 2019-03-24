6e0e7bee7ded5eb2f75d329617426eb72b0fecdb
OgnjenArsenijevic <30389716+OgnjenArsenijevic@users.noreply.github.com>
2019-03-24 19:10:03 +0100

    C++

---
+++ 1023. Binary String With Substrings Representing 1 To N.cpp
@@ -0,0 +1,34 @@
+///1023. Binary String With Substrings Representing 1 To N
+///Author: Ognjen Arsenijevic
+///username: ognjen1998
+class Solution 
+{
+public:
+    unordered_map<int,int> ump;
+    bool queryString(string s, int n) 
+    {
+        for(int i=0;i<s.size();i++)
+        {
+            string ss="";
+            for(int j=i;j<i+30 && j<s.size();j++)
+            {
+                ss+=s[j];
+                int num=0;
+                int st=1;
+                for(int rr=ss.size()-1;rr>=0;rr--)
+                {
+                    if(ss[rr]=='1')
+                        num+=st;
+                    st*=2;
+                }
+                ump[num]++;
+            }
+        }
+        for(int i=1;i<=n;i++)
+        {
+            if(ump[i]==0)
+                return false;
+        }
+        return true;
+    }
+};
