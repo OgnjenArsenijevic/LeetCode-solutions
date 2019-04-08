156be48b2e39fd51993bfa4a48db6f52e6e7f9ff
OgnjenArsenijevic <30389716+OgnjenArsenijevic@users.noreply.github.com>
2019-04-08 04:31:48 +0200

    C++

---
+++ 877. Stone Game.cpp
@@ -0,0 +1,11 @@
+///877. Stone Game
+///Author: Ognjen Arsenijevic
+///username: ognjen1998
+class Solution 
+{
+public:
+    bool stoneGame(vector<int>& piles) 
+    {
+        return true;
+    }
+};
