87b9b36e33918a1360f850f8bc9b7de08479eea9
OgnjenArsenijevic <30389716+OgnjenArsenijevic@users.noreply.github.com>
2019-05-28 00:49:36 +0200

    oraclesql

---
+++ 183. Customers Who Never Order.sql
@@ -0,0 +1,8 @@
+/*
+183. Customers Who Never Order
+Author: Ognjen Arsenijevic
+username: ognjen 1998
+*/
+SELECT NAME AS "CUSTOMERS"
+FROM CUSTOMERS
+WHERE ID NOT IN (SELECT CUSTOMERID FROM ORDERS)
