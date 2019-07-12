d154c734f31e3a0e8f7862968150837fd96af63a
OgnjenArsenijevic <30389716+OgnjenArsenijevic@users.noreply.github.com>
2019-07-12 22:13:06 +0200

    C++

---
+++ 744. Find Smallest Letter Greater Than Target.cpp
@@ -0,0 +1,15 @@
+///744. Find Smallest Letter Greater Than Target
+///Author: Ognjen Arsenijevic
+///username: ognjen1998
+class Solution 
+{
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) 
+    {
+        for(int i=0;i<letters.size();i++)
+        {
+            if(letters[i]>target) return letters[i];
+        }
+        return letters[0];
+    }
+};
