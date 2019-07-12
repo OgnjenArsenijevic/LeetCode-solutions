27f796bd9d280569794e7de1d85c5fafc8c36685
OgnjenArsenijevic <30389716+OgnjenArsenijevic@users.noreply.github.com>
2019-07-12 23:32:49 +0200

    C++

---
+++ 912. Sort an Array.cpp
@@ -0,0 +1,12 @@
+///912. Sort an Array
+///Author: Ognjen Arsenijevic
+///username: ognjen1998
+class Solution 
+{
+public:
+    vector<int> sortArray(vector<int>& v) 
+    {
+        sort(v.begin(),v.end());
+        return v;
+    }
+};
