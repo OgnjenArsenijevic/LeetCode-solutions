c4bb59d168077d756975c150cc9013bfcc74a99c
OgnjenArsenijevic <30389716+OgnjenArsenijevic@users.noreply.github.com>
2019-07-16 03:26:23 +0200

    C++

---
+++ 222. Count Complete Tree Nodes.cpp
@@ -0,0 +1,21 @@
+///222. Count Complete Tree Nodes
+///Author: Ognjen Arsenijevic
+///username: ognjen1998
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    int countNodes(TreeNode* root) 
+    {
+        if(root==NULL) return 0;
+        return countNodes(root->left)+countNodes(root->right)+1;
+    }
+};
