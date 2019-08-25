a109a736be6e74b0ce67c8be873647350522c5b5
OgnjenArsenijevic <30389716+OgnjenArsenijevic@users.noreply.github.com>
2019-08-25 17:49:29 +0200

    C++

---
+++ 287. Find the Duplicate Number.cpp
@@ -0,0 +1,16 @@
+///287. Find the Duplicate Number
+///Author: Ognjen Arsenijevic
+///username: ognjen1998
+class Solution 
+{
+public:
+    int findDuplicate(vector<int>& v) 
+    {
+        sort(v.begin(),v.end());
+        for(int i=1;i<v.size();i++)
+        {
+            if(v[i]==v[i-1]) return v[i];
+        }
+        return -1;
+    }
+};
