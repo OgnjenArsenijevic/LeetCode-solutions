c480f991c7c6fa1f1aa5d4365b8f0d8728f2db36
OgnjenArsenijevic <30389716+OgnjenArsenijevic@users.noreply.github.com>
2019-08-27 03:04:54 +0200

    JAVA

---
+++ 405. Convert a Number to Hexadecimal.java
@@ -0,0 +1,10 @@
+///405. Convert a Number to Hexadecimal
+///Author: Ognjen Arsenijevic
+///username: ognjen1998
+class Solution 
+{
+    public String toHex(int num) 
+    {
+        return Integer.toHexString(num);
+    }
+}
