f3cf60f6d24a5982a95a1b1abf78f99de8b4493e
OgnjenArsenijevic <30389716+OgnjenArsenijevic@users.noreply.github.com>
2019-08-29 03:55:24 +0200

    C++

---
+++ 766. Toeplitz Matrix.cpp
@@ -0,0 +1,19 @@
+///766. Toeplitz Matrix
+///Author: Ognjen Arsenijevic
+///username: ognjen1998
+class Solution 
+{
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& mat) 
+    {
+        for(int i=1;i<mat.size();i++)
+        {
+            for(int j=1;j<mat[i].size();j++)
+            {
+                if(mat[i][j]!=mat[i-1][j-1])
+                    return false;
+            }
+        }
+        return true;
+    }
+};
