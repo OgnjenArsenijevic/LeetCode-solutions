3678ba20b81c77e5dbcee24e90dd5b93d04feace
OgnjenArsenijevic <30389716+OgnjenArsenijevic@users.noreply.github.com>
2020-02-28 20:20:55 +0100

    C++

---
+++ 1313. Decompress Run-Length Encoded List.cpp
@@ -0,0 +1,19 @@
+///1313. Decompress Run-Length Encoded List
+///Author: Ognjen Arsenijevic
+///username: ognjen1998
+class Solution 
+{
+public:
+     vector<int> decompressRLElist(vector<int>& nums)
+     {
+        vector<int> ans;  
+        for(int i=0;i<nums.size();i+=2)
+        {
+            int a=nums[i];
+            int b=nums[i+1];
+            for(int j=0;j<a;j++)
+                ans.push_back(b);
+        }
+        return ans;
+    }
+};
