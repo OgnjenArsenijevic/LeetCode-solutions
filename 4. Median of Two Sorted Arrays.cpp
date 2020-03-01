a55ac58a8a1173aec269f263eb93019cbd10107b
OgnjenArsenijevic <30389716+OgnjenArsenijevic@users.noreply.github.com>
2020-03-01 16:40:02 +0100

    C++

---
+++ 4. Median of Two Sorted Arrays.cpp
@@ -0,0 +1,26 @@
+///4. Median of Two Sorted Arrays
+///Author: Ognjen Arsenijevic
+///username: ognjen1998
+class Solution 
+{
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)
+    {
+        vector<int> v;
+        int i=0, j=0;
+        while(i<nums1.size() && j<nums2.size())
+        {
+            if(nums1[i]<nums2[j])
+                v.push_back(nums1[i++]);
+            else
+                v.push_back(nums2[j++]);
+        }
+        while(i<nums1.size())
+            v.push_back(nums1[i++]);
+        while(j<nums2.size())
+            v.push_back(nums2[j++]);
+        if(v.size()&1)
+            return v[v.size()/2];
+        return (v[v.size()/2]+v[v.size()/2-1])/2.0;
+    }
+};
