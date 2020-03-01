885415167190dbf53dd418db190bd788dd03c74d
OgnjenArsenijevic <30389716+OgnjenArsenijevic@users.noreply.github.com>
2020-03-01 16:42:38 +0100

    C++

---
+++ 10. Regular Expression Matching.cpp
@@ -0,0 +1,10 @@
+///10. Regular Expression Matching
+///Author: Ognjen Arsenijevic
+///username: ognjen1998
+class Solution 
+{
+    public boolean isMatch(String s, String p) 
+    {
+        return s.matches(p);
+    }
+}
