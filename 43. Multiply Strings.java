33d72aec7f7443847e7b29a5c0aa86c6fae899bb
OgnjenArsenijevic <30389716+OgnjenArsenijevic@users.noreply.github.com>
2020-03-01 18:37:49 +0100

    JAVA

---
+++ 43. Multiply Strings.java
@@ -0,0 +1,14 @@
+///43. Multiply Strings
+///Author: Ognjen Arsenijevic
+///username: ognjen1998
+import java.math.*;
+class Solution 
+{
+    public String multiply(String num1, String num2) 
+    {
+        BigInteger n1 = new BigInteger(num1);
+        BigInteger n2 = new BigInteger(num2);
+        n1=n1.multiply(n2);
+        return n1.toString();
+    }
+}
