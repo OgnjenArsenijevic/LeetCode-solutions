8b0c01dff4a8bbcfa0820af73ceaa57dd3c19cca
OgnjenArsenijevic <30389716+OgnjenArsenijevic@users.noreply.github.com>
2020-03-07 00:46:55 +0100

    C++

---
+++ 359. Logger Rate Limiter.cpp
@@ -0,0 +1,29 @@
+///359. Logger Rate Limiter
+///Author: Ognjen Arsenijevic
+///username: ognjen1998
+class Logger 
+{
+public:
+    /** Initialize your data structure here. */
+    unordered_map<string,int> ump;
+    Logger() 
+    {    
+    }
+    
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. */
+    bool shouldPrintMessage(int timestamp, string message) 
+    {
+        if(!(ump.find(message)==ump.end() || timestamp-ump[message]>=10))
+            return false;
+        ump[message]=timestamp;
+        return true;
+    }
+};
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger* obj = new Logger();
+ * bool param_1 = obj->shouldPrintMessage(timestamp,message);
+ */
