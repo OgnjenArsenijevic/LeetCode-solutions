1d5271916cb23ae2b12d572e017c5282797f8705
OgnjenArsenijevic <30389716+OgnjenArsenijevic@users.noreply.github.com>
2020-03-07 00:51:03 +0100

    C++

---
+++ 339. Nested List Weight Sum.cpp
@@ -0,0 +1,50 @@
+///339. Nested List Weight Sum
+///Author: Ognjen Arsenijevic
+///username: ognjen1998
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *     // Constructor initializes an empty nested list.
+ *     public NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     public NestedInteger(int value);
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     public void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     public void add(NestedInteger ni);
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+class Solution
+{
+    public int solve(List<NestedInteger> list, int d)
+    {
+        int ans=0;
+        for(NestedInteger i : list)
+        {
+            if(i.isInteger())
+                ans+=i.getInteger()*d;
+            else
+                ans+=solve(i.getList(),d+1);
+        }
+        return ans;
+    }
+    public int depthSum(List<NestedInteger> nestedList)
+    {
+        return solve(nestedList,1);
+    }
+}
