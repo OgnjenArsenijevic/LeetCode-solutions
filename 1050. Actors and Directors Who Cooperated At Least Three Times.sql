2e4ee6103f70f9b18ca6578d2666e37395a2ab29
OgnjenArsenijevic <30389716+OgnjenArsenijevic@users.noreply.github.com>
2020-03-17 23:59:39 +0100

    MySQL

---
+++ 1050. Actors and Directors Who Cooperated At Least Three Times.sql
@@ -0,0 +1,9 @@
+/*
+1050. Actors and Directors Who Cooperated At Least Three Times
+Author: Ognjen Arsenijevic
+username: ognjen1998
+*/
+SELECT actor_id, director_id
+FROM ActorDirector
+GROUP BY actor_id, director_id
+HAVING COUNT(timestamp) >= 3;
